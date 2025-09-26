434fcbef7d23a510b1dd3bd4b0b5726d660e1dad
Frankminiel <167045637+Frankminiel@users.noreply.github.com>
2025-09-26 15:33:18 -0400

    Trask 1 by Frank Miniel2025-0977

---
+++ Trask 1/current time.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("la fecha y hora actual es: " + DateTime.Now);
+//Frank Miniel 2025-0977
---
+++ Trask 1/float.cs
@@ -0,0 +1,5 @@
+﻿        float n = 10152466.25f;
+        byte r = (byte)(5 + n);
+        Console.WriteLine("el valor del float es : " + n);
+        Console.WriteLine("aqui el resultado en byte: " + r);
+//Frank Miniel 2025-0977
