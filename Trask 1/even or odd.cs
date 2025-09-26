6defc9e2534d14277ec3bf6c90604ab4c80b50c9
Frankminiel <167045637+Frankminiel@users.noreply.github.com>
2025-09-26 15:13:53 -0400

    task 1 By Franck Miniel4

---
+++ Trask 1/even or odd.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Coloque un valor:");
+int n;
+n=int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{ Console.WriteLine("El valor es par");}
+else { Console.WriteLine("El valor es impar"); }
+//Frank Miniel 2025-0977
