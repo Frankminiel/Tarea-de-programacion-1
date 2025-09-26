5240989af7b7cda1ad3fb82ce19ecbd46970fd98
Frankminiel <167045637+Frankminiel@users.noreply.github.com>
2025-09-26 15:31:37 -0400

    Delete Trask 1 directory

--- Trask 1/even or odd.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Coloque un valor:");
-int n;
-n=int.Parse(Console.ReadLine());
-if (n % 2 == 0)
-{ Console.WriteLine("El valor es par");}
-else { Console.WriteLine("El valor es impar"); }
-//Frank Miniel 2025-0977
--- Trask 1/float.cs
+++
@@ -1,5 +0,0 @@
-﻿        float n = 10152466.25f;
-        byte r = (byte)(5 + n);
-        Console.WriteLine("el valor del float es : " + n);
-        Console.WriteLine("aqui el resultado en byte: " + r);
-//Frank Miniel 2025-0977
